214412a5081f3de14fdca8fc8299ac9b3a231d07
Aaron <aaron1venema@gmail.com>
2023-01-28 20:43:54 -0600

    Update Card.js

--- src/Card.js
+++ src/Card.js
@@ -8,7 +8,7 @@ const Card = (props) => {
                     <img src="./assets/location.png" alt="location" className="location-icon"/>
                     <h4 className="location">{props.country.toUpperCase()}</h4>
                     <a 
-                        href="https://www.google.com/maps/d/viewer?mid=1lilT6Fjy0yVQ6Fr9d4DLUw2aDzo&hl=en&ll=10.144635127374483%2C-84.11956799999999&z=9" 
+                        href={props.googleHref} 
                         className="google"
                         target="_blank"
                         rel="noreferrer"
